82ab7f367347171d2f3f0fcb3ae1165602016d01
LordIdra <percy.baker.0@gmail.com>
2025-02-13 02:27:47 +0000

    fix shit

---
+++ migrate/run.py
@@ -0,0 +1,20 @@
+import mysql.connector
+import sys
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user=sys.argv[1],
+    password=sys.argv[2],
+    database="recipe",
+)
+
+cursor = mydb.cursor()
+
+cursor.execute("SELECT link, domain, priority FROM waiting_link")
+
+result = cursor.fetchall()
+
+print(result.rowcount)
+for row in result:
+    print(row[0])
+
