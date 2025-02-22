464df8f77dfd9b1a3ee393d91ff648e92f159040
LordIdra <percy.baker.0@gmail.com>
2025-02-22 17:11:13 +0000

    bruh

--- recipe-api/src/main.rs
+++ recipe-api/src/main.rs
@@ -62,7 +62,8 @@ async fn main() {
     };
 
     let api_router = OpenApiRouter::new()
-        .routes(routes!(submit_link, parse_ingredients))
+        .routes(routes!(submit_link))
+        .routes(routes!(parse_ingredients))
         .with_state(state);
 
 
