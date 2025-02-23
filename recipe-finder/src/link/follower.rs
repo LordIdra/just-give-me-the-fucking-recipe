be9318eb7a52ed1d9ca23204980d9e87de41bf40
LordIdra <percy.baker.0@gmail.com>
2025-02-23 00:25:58 +0000

    bruh

--- recipe-finder/src/link/follower.rs
+++ recipe-finder/src/link/follower.rs
@@ -18,19 +18,32 @@ static HREF_REGEX: LazyLock<Regex> = LazyLock::new(||
 
 #[tracing::instrument(skip(contents))]
 pub async fn follow(contents: String, link: String) -> Vec<String> {
+    let url = Url::parse(&link)
+        .expect("How did you screw this up");
+    let domain = url.domain()
+        .expect("How did you screw this up");
+
     LINK_ELEMENT_REGEX.captures_iter(&contents)
         .map(|captures| captures.get(0).unwrap().as_str())
         .inspect(|v| trace!("{}", v))
         .filter_map(|element| HREF_REGEX.captures(element))
             .map(|captures| captures.get(1).unwrap().as_str())
+
         // fix relative links (eg '/category/stupid_recipes' -> bbc.co.uk/category/stupid_recipes)
-        .map(|v| if v.chars().next().is_some_and(|v| v == '/') { link.clone() + v } else { v.to_string() })
+        .map(|v| if v.chars().next().is_some_and(|v| v == '/') { 
+                domain.to_string() + v 
+            } else { 
+                v.to_string() 
+            })
         .filter(|new_link| Url::parse(new_link).is_ok())
+
         // eg, bruh.com/some-recipe might have links to bruh.com/some-recipe/comments#36
         .filter(|new_link| !new_link.starts_with(&link))
+
         // hardcoded fix. often, recipes have www.domain.com/your_shitty_recipe/wprm_print pages
         // these pages have shit schemas. but the original pages are generally fine
         .map(|v| v.replace("/wprm_print", ""))
+
         .map(|v| v.to_owned())
         .collect()
 }
